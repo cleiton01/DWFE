71aadad8017a5811b97194ac9620e56e0b67e02a
cleiton01 <clleiton.silva@gmail.com>
2017-12-29 01:27:17 -0200

    ataualizado

--- com/project-pixel-art-maker-starter-master/js/designs.js
+++ com/project-pixel-art-maker-starter-master/js/designs.js
@@ -1,10 +1,17 @@
-function makeGrid() {
+function makeGrid(event) {
+    event.preventDefault();
 
+    // this code to remove the contect of the table if table is alredy created.
+    let remContentTable, remRableRow;
+    remContentTable = $('#pixel_canvas');
+    remRableRow = $('.grid');
+    console.log("remRableRow: "+remRableRow.val()   );
+    remRableRow.remove();
+
+
+    // This code create a responsive grid to draw
     let getInputHeight, getInputWidth, cellTable;
 
-    let family2 = $('<div id="1"><h1>Hello</h1></div>');
-
-    console.log("TESTE");
 
     getInputHeight = $('#input_height').val();
     getInputWidth =  $('#input_width').val();
@@ -14,18 +21,38 @@ function makeGrid() {
 
     cellTable = $('#pixel_canvas');
 
-    for ( let tableWidth = 0; tableWidth <= getInputWidth; tableWidth++){
-        family2.insertAfter(cellTable);
+    let setRow, setColumn, row_;
+    for ( let tableHeight = 0; tableHeight < getInputHeight; tableHeight++ ){
+        row_ = cellTable.append('<tr id="grid-height"></tr>');
+        for ( let tableWidth = 0; tableWidth < getInputWidth; tableWidth++ ){
+            row_.append('<td class="grid"></td>');
+        }
     }
 
 
-    var vali1;
-
-    vali1 = $('#pixel_canvas');
-    vali1.append('<img src="http://placepuppy.it/200/300">');
-    document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+    $(".grid").width(800/getInputHeight);
+    $(".grid").height(800/getInputHeight);
 
 
+
+
+
+    // This code change de color used to draw on the panel.
+
+    let colorValue;
+    colorValue = $('#colorPicker').val();
+    console.log(colorValue);
+
+    $(".grid").mouseover(function() {
+        $(this).css("background-color", "black");
+    });
+
+    $(".newGrid").click(function() {
+        refreshGrid();
+
+        $(".grid").mouseover(function() {
+            $(this).css("background-color", "black");
+        });
+    });
 
 }
-
